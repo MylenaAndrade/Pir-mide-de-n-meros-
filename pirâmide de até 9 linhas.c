aab0095118912b7c46565218e23075caa08ab9cc
Mylena <102628246+MylenaAndrade@users.noreply.github.com>
2022-06-10 17:13:12 -0300

    Update pirâmide de até 9 linhas.c

--- pirâmide de até 9 linhas.c
+++ pirâmide de até 9 linhas.c
@@ -29,39 +29,39 @@ void main(){
 
 //função
 void triangulo(int n){
-	//declartação de variáveis
-	int i, j, novo, aux;
+//declartação de variáveis
+int i, j, novo, aux;
 	
-	//varrendo até o número antecessor em que o usuário for digitar
-		for( i=0; i<n ; i++)
-			{
-				//pulando espaços para fazer o formato da pirâmide
-				for( j=n-i; j>=1 ; j--)
-				{
-					printf("  ");
-		    	}
-		    	//varrendo a primeira parte da pirâmide
-		    	aux= i+1;
-				for( j=1; j<=aux ; j++)
-				{
-					
-					printf("%d ", j);
-				//verificando o maior numero que j pode atingir
-				  if(j>i)
-					{
-						//diminuindo 1 do 'J', para formar a ordem decrescente do maior 'J' até 1
-						novo=j;
-						while(novo!= 1){
-							novo--;
-							printf("%d ", novo);
-						}
+//varrendo até o número antecessor em que o usuário for digitar
+ for( i=0; i<n ; i++)
+ {
+	
+//pulando espaços para fazer o formato da pirâmide
+  for( j=n-i; j>=1 ; j--)
+  {
+       printf("  ");
+  }
+//varrendo a primeira parte da pirâmide
+	aux= i+1;
+	for( j=1; j<=aux ; j++)
+	{
+	      printf("%d ", j);		
+//verificando o maior numero que j pode atingir
+		if(j>i)
+		{
+//diminuindo 1 do 'J', para formar a ordem decrescente do maior 'J' até 1
+		   novo=j;
+		   while(novo!= 1){
+		   	novo--;
+			printf("%d ", novo);
+		   }
 						
-					}
+		}
 					
-				}
-				printf("\n");
+	}
+		printf("\n");
 				
-			}
+ }
 	
 			
 }
